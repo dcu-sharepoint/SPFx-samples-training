be786ec3ed657300a4c7f463a47f373acdd72f6c
Daniel Gaeta <gaeta.d@gmail.com>
2016-09-02 12:28:19 -0700

    added comments to explain approach

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -13,53 +13,67 @@ import styles from './HelloWorld.module.scss';
 import * as strings from 'mystrings';
 import { IHelloWorldWebPartProps } from './IHelloWorldWebPartProps';
 
-import {Component, Directive, ViewContainerRef, ViewEncapsulation, ComponentResolver} from 'angular2/core';
+import {Component, Directive, ViewContainerRef, ComponentResolver} from 'angular2/core';
 import {bootstrap}    from 'angular2/platform/browser';
 
 export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
 
   public constructor(context: IWebPartContext) {
     super(context);
-    bootstrap( Component({
-      selector: 'my-app-' + this.context.instanceId,
-      encapsulation: ViewEncapsulation.Emulated,
-      template:
-        `<p>Dynamic Component {{message}}</p>
-        <button (click)="addTodo()">Click</button>
-         <ul>
-          <li *ngFor="let todo of todos">
-            {{ todo }}
-          </li>
-         </ul>
-        `
-     })((class AppComponent {
-          public todos: string[];
-          public message: string;
+    bootstrap(
+      // The bootstrap function accepts any contructable object as a parameter.
 
-          constructor(private vcRef: ViewContainerRef, private resolver: ComponentResolver) {
-            this.todos = ['task1', 'task 2'];
-            this.message = "Good morning!";
-            console.log('annotations');
-            console.log(Reflect.getMetadata('annotations', AppComponent));
-            // console.log('design:paramtypes');
-            // console.log(Reflect.getMetadata('design:paramtypes', AppComponent));
-            // console.log('propMetadata');
-            // console.log(Reflect.getMetadata('propMetadata', AppComponent));
-            // console.log('parameters');
-            // console.log(Reflect.getMetadata('parameters', AppComponent));
-            //Reflect.defineMetadata('annotations', metadataValue, C.prototype, "method");
+      // First we must decorate the constructable object (which will serve as our Component)
+      // with metadata that tells Angular2 which CSS Selector to look for in the HTML, and how to render
+      // the Component's view inside the CSS Selector's element.
+      Component(
+        {
+          selector: 'my-app-' + this.context.instanceId,
+          template:
+            `<p>Dynamic Component {{message}}</p>
+            <button (click)="addTodo()">Click</button>
+            <ul>
+              <li *ngFor="let todo of todos">
+                {{ todo }}
+              </li>
+            </ul>
+            `
+        }
+      )
+      // Now we must define a constructable object that will be our Component.
+      // We wrap the constructable object in parenthesis to explicitly tell the parser to
+      // expect an expression and not a declaration.
+      // ***But more importantly, we wrap with parenthesis so that we can the IIFE
+      // object model that is transpiled from the class declaration is
+      // lexically scoped in this WebPart instance.
+      (
+        // This class gets transpiled to a JavaScript IIFE containing an object model
+        class AppComponent {
 
-          }
+        public todos: string[];
+        public message: string;
 
-          public addTodo(): void {
-            console.log('in addTodo');
-            this.todos.push('feature 1');
-            console.log(this.todos);
-          };
-        })
+        constructor(private vcRef: ViewContainerRef, private resolver: ComponentResolver) {
+          this.todos = ['task1', 'task 2'];
+          this.message = "Good morning!";
+          console.log('annotations');
+          console.log(Reflect.getMetadata('annotations', AppComponent));
+          // console.log('design:paramtypes');
+          // console.log(Reflect.getMetadata('design:paramtypes', AppComponent));
+          // console.log('propMetadata');
+          // console.log(Reflect.getMetadata('propMetadata', AppComponent));
+          // console.log('parameters');
+          // console.log(Reflect.getMetadata('parameters', AppComponent));
+        }
+
+        public addTodo(): void {
+          console.log('in addTodo');
+          this.todos.push('feature 1');
+          console.log(this.todos);
+        };
+        }
       )
     ).then(app => {
-        //app['_hostElement'].nativeElement =  `<div class="Ng2"><h1>Angular 2-${this.context.instanceId}</h2><my-app-${this.context.instanceId}/></div>`
         console.log(app['_hostElement'].nativeElement);
         console.log('Bootstrap Successful');
         console.log(app);
