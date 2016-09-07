ae7b66196e75f37cc1d08fc916e9e07cdc90f9b4
Manish Garg <manishga@microsoft.com>
2016-09-07 16:16:22 -0700

    ngModule working with @ngModule directive

--- src/webparts/helloWorld/ListComponent.ts
+++ src/webparts/helloWorld/ListComponent.ts
@@ -5,23 +5,23 @@ import {Component, Input} from '@angular/core';
 @Component({
       selector: `list-component`,
       template:
-      `<button (click)="addTodo()">Click</button>`
-      //  <ul>
-      //    <li *ngFor="let todo of todos">
-      //      {{ todo }}
-      //    </li>
-      //  </ul>
+      `<button (click)="addTodo()">Click</button>
+        <ul>
+          <li *ngFor="let todo of todos">
+            {{ todo }}
+          </li>
+        </ul>`
     })
 export default class ListComponent {
-  // @Input()
-  // private todos: string[];
+  @Input()
+  todos: string[];
 
   constructor() {
-    console.log('in here');
+    console.log('*** ListComponent constructor ***');
    // this.todos = todos;
   }
 
   public addTodo(): void {
-  //  this.todos.push('feature 1');
+   this.todos.push('feature 1');
   };
 }
